06a5c9c10c3e3c1f3ec41d9d1e61623aa1cc6f9c
João Santos <joaosantosdev97@gmail.com>
2023-02-02 23:06:54 -0300

    Money and defence

--- src/main.rs
+++ src/main.rs
@@ -21,21 +21,32 @@ fn main() -> std::io::Result<()> {
     //     None => println!("User did not select anything"),
     // }
 
+    // Read commands
+
     //fight simulation
+
     let mut players_health = 170;
     let mut boss_helth = 250;
 
     let mut player_hit_counter = 0;
     let mut boss_hit_counter = 0;
+    let mut player_balance: f64;
 
     loop {
         let player_hit_damage = player_hit();
-        let boss_hit_damage = boss_hit();
+        // let boss_hit_damage = boss_hit()[0];
+        let boss_hit_damage = 0;
+        let boss_reward: u32 = 10000;
 
         if boss_helth > player_hit_damage {
             player_hit_counter += 1;
-            boss_helth -= player_hit_damage;
-            println!("Boss has now {}hp \n", boss_helth);
+
+            if !has_boss_defended(20) {
+                boss_helth -= player_hit_damage;
+                println!("Boss has now {}hp \n", boss_helth);
+            } else {
+                println!("Boss defended a {} git \n", player_hit_damage);
+            }
 
             if players_health > boss_hit_damage {
                 players_health -= boss_hit_damage;
@@ -49,9 +60,10 @@ fn main() -> std::io::Result<()> {
                 );
             }
         } else {
+            player_balance = boss_reward as f64 / player_hit_counter as f64;
             break println!(
-                "Boss is now dead! Killed with {} hits \n",
-                player_hit_counter
+                "Boss is now dead! Killed with {} hits. Player balance is now {:.2} \n",
+                player_hit_counter, player_balance
             );
         }
     }
@@ -62,10 +74,20 @@ fn main() -> std::io::Result<()> {
         x % 60 + 10 // 10 to 40 hit points
     }
 
-    fn boss_hit() -> u32 {
+    fn boss_hit() -> (u32, bool) {
         let mut rng = rand::thread_rng();
         let x: u32 = rng.gen();
-        x % 50 // 0 to 50 hit points
+        // 0 to 50 hit points
+        let hit = x % 50;
+        let defended = false;
+        (x % 50, false)
+    }
+
+    fn has_boss_defended(chance: u32) -> bool {
+        let mut rng = rand::thread_rng();
+        let total_range: u32 = rng.gen();
+        let defence_number = total_range % 100;
+        defence_number <= chance
     }
 
     Ok(())
